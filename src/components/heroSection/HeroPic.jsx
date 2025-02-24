2db39803ee0488343c3298b16edfdec8ea607df9
abdulhadii123 <hadi.woodenclouds@gmail.com>
2025-02-24 16:44:27 +0530

    fifth commit

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/images/me-01.png"
+        src="/images/me.png"
         alt="Abdul hadi"
         className="max-h-[450px] w-auto"
       />
